47aa1c505a75f452549ba92ec7daf2975d045b3e
Yash Sangwan <yashsangwan4700@gmail.com>
2024-07-16 15:42:21 +0530

    added the graphics folder in the project

--- main.py
+++ main.py
@@ -4,12 +4,18 @@ from sys import exit
 pygame.init()
 screen = pygame.display.set_mode((800,400))
 pygame.display.set_caption('Mario clone')
+clock = pygame.time.Clock()
+sky_surface = pygame.image.load('Basic-pygame-project/graphics/Sky.png')
+ground_surface=pygame.image.load('Basic-pygame-project/graphics/ground.png')
 
 while True:
+    #we draw all our elements here
+    #and then update everythin
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
-            exit()            
-    #we draw all our elements here
-    #and then update everythin
+            exit()    
+    screen.blit(sky_surface,(0,0))    
+    screen.blit(ground_surface,(0,300))    
     pygame.display.update()
+    clock.tick(60)
