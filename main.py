11cc63e44db99ab5727562275acb1cc9661553b1
Yash Sangwan <yashsangwan4700@gmail.com>
2024-07-16 13:51:03 +0530

    added some boileplate code of pygame

--- main.py
+++ main.py
@@ -1,2 +1,15 @@
 import pygame
-# this is a test of git integration
+from sys import exit
+
+pygame.init()
+screen = pygame.display.set_mode((800,400))
+pygame.display.set_caption('Mario clone')
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()            
+    #we draw all our elements here
+    #and then update everythin
+    pygame.display.update()
