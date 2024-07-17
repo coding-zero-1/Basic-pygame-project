a322fa8f734cd187d0ae933d07d93b1341bb93fe
Yash Sangwan <yashsangwan4700@gmail.com>
2024-07-17 13:12:41 +0530

    added some text_surface

--- main.py
+++ main.py
@@ -5,8 +5,11 @@ pygame.init()
 screen = pygame.display.set_mode((800,400))
 pygame.display.set_caption('Mario clone')
 clock = pygame.time.Clock()
+
 sky_surface = pygame.image.load('Basic-pygame-project/graphics/Sky.png')
 ground_surface=pygame.image.load('Basic-pygame-project/graphics/ground.png')
+test_font = pygame.font.Font(None,50)
+test_surface = test_font.render('My game',False,'Green')
 
 while True:
     #we draw all our elements here
@@ -16,6 +19,8 @@ while True:
             pygame.quit()
             exit()    
     screen.blit(sky_surface,(0,0))    
-    screen.blit(ground_surface,(0,300))    
+    screen.blit(ground_surface,(0,300)) 
+    screen.blit(test_surface,(300,50)) 
+      
     pygame.display.update()
     clock.tick(60)
