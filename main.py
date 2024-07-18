c9cd474c9a6baef9cf01bf9c7e3f3d02ad0e3a98
Yash Sangwan <yashsangwan4700@gmail.com>
2024-07-18 10:13:11 +0530

    added some extra things in main.py

--- main.py
+++ main.py
@@ -3,13 +3,15 @@ from sys import exit
 
 pygame.init()
 screen = pygame.display.set_mode((800,400))
-pygame.display.set_caption('Mario clone')
+pygame.display.set_caption('Infinite runner')
 clock = pygame.time.Clock()
+score_value='0'
 
 sky_surface = pygame.image.load('Basic-pygame-project/graphics/Sky.png').convert()
 ground_surface=pygame.image.load('Basic-pygame-project/graphics/ground.png').convert()
 test_font = pygame.font.Font('Basic-pygame-project/font/Pixeltype.ttf',50)
 test_surface = test_font.render('My game',False,'Black')
+
 snail_surface = pygame.image.load('Basic-pygame-project/graphics/snail/snail1.png').convert_alpha()
 snail_rectange=snail_surface.get_rect(midbottom=(700,300))
 # snail_position=700
@@ -27,8 +29,8 @@ while True:
     screen.blit(ground_surface,(0,300)) 
     screen.blit(test_surface,(300,50)) 
     screen.blit(snail_surface,snail_rectange)
-    snail_rectange.left-=3
-    if snail_rectange.left<0:
+    snail_rectange.left-=4
+    if snail_rectange.right<0:
         snail_rectange.right=850
     '''
     snail_position-=3
@@ -37,5 +39,7 @@ while True:
     screen.blit(player_surface,player_rectangle)
     player_rectangle.left+=2
     
+    if player_rectangle.colliderect(snail_rectange):
+        print('Collision')
     pygame.display.update()
     clock.tick(60)
